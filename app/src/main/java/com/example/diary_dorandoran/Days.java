48faaadcaf958e7e792ae19165005ad8df1a4dd6
ImJaeYeeon <w2109@e-mirim.hs.kr>
2022-11-22 18:50:36 +0900

    배경 수정

--- app/src/main/java/com/example/diary_dorandoran/Days.java
+++ app/src/main/java/com/example/diary_dorandoran/Days.java
@@ -14,10 +14,18 @@ public class Days extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_days);
         ImageView todobtn = (ImageView) findViewById(R.id.todobtn);
+        ImageView diarybtn = (ImageView) findViewById(R.id.diarybtn);
         todobtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(),todolist.class);
+                startActivity(intent);
+            }
+        });
+        diarybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),Diary.class);
                 startActivity(intent);
             }
         });
