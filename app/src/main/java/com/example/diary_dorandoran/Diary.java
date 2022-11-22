8cbb592473a7491c97453d6df595b2e5a0eeb88d
ImJaeYeeon <w2109@e-mirim.hs.kr>
2022-11-22 22:51:58 +0900

    스트레스 지수

--- app/src/main/java/com/example/diary_dorandoran/Diary.java
+++ app/src/main/java/com/example/diary_dorandoran/Diary.java
@@ -3,12 +3,33 @@ package com.example.diary_dorandoran;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Diary extends AppCompatActivity {
+    TextView stresstv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+        stresstv = findViewById(R.id.stresstv);
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                stresstv.setText(String.format("오늘의 스트레스 지수는 %d 입니다",seekBar.getProgress()));
+            }
+        });
     }
 }
